76958ca502e68a6e57b79ff0f22ecd1ce2c5a023
photon-delight <greenelectronharvest@gmail.com>
2021-06-22 23:09:02 +0200

    changed line endings to windows style

--- csv_import.py
+++ csv_import.py
@@ -9,14 +9,10 @@ filename = sys.argv[1]
 
 export_filename = filename.removesuffix('.csv') + "_sage.csv"
 
-df = pd.read_csv(filename)
-
-print(df)
+df = pd.read_csv(filename, parse_dates=['Date'])
 
 del df['Balance']
 
-df['Date'] = pd.to_datetime(df.Date, format="%Y%m%d")
+print(df)
 
-print (df)
-
-df.to_csv(export_filename, index=False)
+df.to_csv(export_filename, index=False, line_terminator='\r\n', date_format="%d/%m/%Y")
